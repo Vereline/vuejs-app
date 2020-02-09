2a1129ac9088674a0d05b9a4c395e2dee66d704f
Viktoryia Stanko <vstanko1998@gmail.com>
2020-02-09 15:13:00 +0300

    WIP about page

--- frontend/vuejs_app/src/components/about/About.vue
+++ frontend/vuejs_app/src/components/about/About.vue
@@ -1,6 +1,49 @@
 <template>
-  <div>
-    <p> About page</p>
+  <div class="container">
+    <div class="wrapper row2">
+  <div id="container" class="clear">
+    <div id="about-us" class="clear">
+      <section id="about-intro" class="clear">
+        <h2>About Us</h2>
+        <img src="https://via.placeholder.com/960x300" alt="">
+        <p>In sed neque id libero pretium luctus. Vivamus faucibus. Ut vitae elit. In hac habitasse platea dictumst. Proin et nisl ac orci tempus luctus. Aenean lacinia justo at nisi. Vestibulum sed eros sit amet nisl lobortis commodo. Suspendisse nulla. Vivamus ac lorem. Aliquam pulvinar purus at felis. Quisque convallis nulla id ipsum. Praesent vitae urna. Fusce blandit nunc nec mi. Praesent vestibulum hendrerit ante.</p>
+        <p>Vivamus accumsan. Donec molestie pede vel urna. Curabitur eget sem ornare felis gravida vestibulum.Sed pulvinar, tellus in venenatis vehicula, lorem magna dignissim erat, in accumsan ante lorem sit amet lorem.</p>
+      </section>
+      <section id="team">
+        <h2>Our Team</h2>
+        <ul class="clear">
+          <li class="one_third first">
+            <figure><img src="https://via.placeholder.com/300" alt="">
+              <figcaption>
+                <p class="team_name">Name Goes Here</p>
+                <p class="team_title">Job Title Here</p>
+                <p class="team_description">Vestassapede et donec ut est liberos sus et eget sed eget. Quisqueta habitur augue magnisl magna phas ellus sagit titor ant curabi turpis.</p>
+              </figcaption>
+            </figure>
+          </li>
+          <li class="one_third">
+            <figure><img src="https://via.placeholder.com/300" alt="">
+              <figcaption>
+                <p class="team_name">Name Goes Here</p>
+                <p class="team_title">Job Title Here</p>
+                <p class="team_description">Vestassapede et donec ut est liberos sus et eget sed eget. Quisqueta habitur augue magnisl magna phas ellus sagit titor ant curabi turpis.</p>
+              </figcaption>
+            </figure>
+          </li>
+          <li class="one_third">
+            <figure><img src="https://via.placeholder.com/300" alt="">
+              <figcaption>
+                <p class="team_name">Name Goes Here</p>
+                <p class="team_title">Job Title Here</p>
+                <p class="team_description">Vestassapede et donec ut est liberos sus et eget sed eget. Quisqueta habitur augue magnisl magna phas ellus sagit titor ant curabi turpis.</p>
+              </figcaption>
+            </figure>
+          </li>
+        </ul>
+      </section>
+    </div>
+  </div>
+</div>
   </div>
 </template>
 
@@ -14,6 +57,5 @@
     }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
 </style>
