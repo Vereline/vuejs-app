d037aa829e3ea6700bdf6a3667288046460ce5ab
Victoria Stanko <vstanko1998@gmail.com>
2020-03-14 22:20:53 +0300

    Create protected routes

--- frontend/vuejs_app/src/router.js
+++ frontend/vuejs_app/src/router.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
+import store from './store';
+import axios from 'axios';
+import {API_URL} from "./constants";
+
 import Main from './components/main/Main'
 import Events from './components/events/Events'
 import Locations from './components/locations/Locations'
@@ -9,14 +14,14 @@ import BlogDetail from './components/blogDetail/blogDetail'
 import CreateBlog from './components/createBlog/createBlog'
 import Blog from './components/blog/Blog'
 import Heroes from './components/heroes/Heroes'
-import Login from './components/login/Login'
-import Signup from './components/signup/Signup'
+// import Login from './components/login/Login'
+// import Signup from './components/signup/Signup'
 import NotFound from './components/notFound/notFound'
 import personProfile from './components/personProfile/personProfile'
 
 Vue.use(Router);
 
-export default new Router({
+const router =  new Router({
   mode: 'history',
   routes: [
     {
@@ -64,16 +69,16 @@ export default new Router({
       name: 'Heroes',
       component: Heroes
     },
-    {
-      path: '/signup',
-      name: 'Signup',
-      component: Signup
-    },
-    {
-      path: '/login',
-      name: 'Login',
-      component: Login
-    },
+    // {
+    //   path: '/signup',
+    //   name: 'Signup',
+    //   component: Signup
+    // },
+    // {
+    //   path: '/login',
+    //   name: 'Login',
+    //   component: Login
+    // },
     {
       path: '/person/:id',
       name: 'PersonProfile',
@@ -85,4 +90,48 @@ export default new Router({
       component: NotFound
     },
   ]
-})
+});
+
+router.beforeEach((to, from, next) => {
+  let allowedRoutes = ["Main", "About", "NotFound"];
+
+  if(allowedRoutes.includes(to.name)){
+    next();
+  } else {
+    let token = store.state.token;
+
+    if (!token) {
+      router.push({name:'Main'});
+      return
+    }
+
+    // validate token
+    const data = {
+      'token': token,
+    };
+    const url = 'accounts/token-verify';
+    const options = {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+        'Authorization': `Bearer ${token}`,
+      },
+      data: data,
+      url: `${API_URL}/${url}/`,
+    };
+    axios.post(options.url, options.data, options)
+      .then(response => {
+        if(response.status === 201) {
+          store.commit('setToken', response.data['token']);
+          next();
+        } else {
+          router.push({name:'Main'});
+        }
+      }).catch(response => {
+        console.log(response);
+        router.push({name:'Main'});
+    });
+  }
+});
+
+export default router
